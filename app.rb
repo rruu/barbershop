c6a8521169e0e61c61600e1bb554f5fccb2ef727
ru <git@xoyo.ru>
2018-04-07 21:54:14 +0300

    add sqlite3

--- app.rb
+++ app.rb
@@ -4,14 +4,18 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+def get_db
+    db = SQLite3::Database.new 'barbershop.db'
+    db.results_as_hash = true
+    return db
+end
+
+
+def seed_db
+
 configure do
 
-    #@db = SQLite3::Database.new 'barbershop.db'
-    #db = get_db
-    #db.results_as_hash = true
-
-    db = SQLite3::Database.new 'barbershop.db'
-
+    db = get_db
     db.execute 'CREATE TABLE IF NOT EXISTS "Users" (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name varchar NOT NULL,
@@ -21,8 +25,20 @@ configure do
         barber varchar NOT NULL
     );'
 
+
+    db.execute 'CREATE TABLE IF NOT EXISTS "Barbers" (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name varchar NOT NULL
+    );'
+
 end
 
+
+def barbers db, name
+    db = get_db
+    db.execute 'select * from Users order by id desc;'
+    return @barbers
+end
 
 get '/' do
     @title = 'Барбершоп - Главная страница'
@@ -32,7 +48,7 @@ end
 get '/visit' do
     @title = 'Барбершоп - Записаться'
     @h1 = 'Запись на посещение'
-    @add_header = true
+    @datetimepicker = true
     erb :visit
 end
 
@@ -48,9 +64,17 @@ get '/barbers' do
     erb :barbers
 end
 
+get '/list' do
+    @title = 'Барбершоп - Лист записи'
+    @h1 = 'Лист записи'
+
+    db = get_db
+    @result = db.execute 'select * from Users order by id desc'
+
+    erb :list
+end
+
 post '/visit' do
-
-
 
     @name = params[:name]
     @date = params[:date]
@@ -60,13 +84,14 @@ post '/visit' do
 
     @complete_to = "Поздравляем #{@name}, вы записались к барберу #{@barber}!"
 
-
+        # Запись информвции о заказе в файл
         # open('public/contacts.txt', 'a') do |f|
         #     #f << "\nName: #{@name}, date: #{@date}, phone:#{@phone}, email:#{@email}, barber:#{@barber}"
         #     hh = {"Name"=> @name, "date"=> @date, "phone"=>@phone, "email"=>@email, "barber"=>@barber}
         #     f << hh
         # end
 
+        db = get_db
         db.execute 'insert into Users (name, datestamp, phone, email, barber) values (?,?,?,?,?)', [@name,@date,@phone,@email,@barber] 
 
       erb :visit
@@ -75,8 +100,14 @@ post '/visit' do
         return erb :visit
       end
 
+# get '/list' do
+#     db = get_db
+#     @resitls = db.execute 'select * from Users order by id desc'
+
+#     erb :list
+
+#     end
+
+
 end
 
-def db
-    return SQLite3::Database.new 'barbershop.db'
-end
